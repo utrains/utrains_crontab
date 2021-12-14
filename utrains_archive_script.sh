7127ff304865831359f23a213f2a5af2c9a2cde5
hermann90 <hermannchefouet@gmail.com>
2021-12-14 23:14:33 +0400

    udate lab

--- utrains_archive_script.sh
+++ utrains_archive_script.sh
@@ -7,17 +7,17 @@
 # created after a certain time (5 minutes) in a folder.
 # we will then archive the folder, then delete it and only keep the archive
 
-DIRECTORY = "/tmp/utrains_logs"
-ARCHIVEDDIR=""
-if [ -d "$DIRECTORY" ]; then
-  # Control will enter here if $DIRECTORY does exist.
+
+DIR="/tmp/utrains_logs"
+if [ -d "$DIR" ]; then
+  # Control will enter here if $DIR exist.
   cd /tmp/utrains_logs
 
   MYDATE=$(date "+%b_%d_%Y_%H.%M.%S")
   mkdir utrains_arch_cron-$MYDATE
   mv utrains_des_* utrains_arch_cron-$MYDATE
 
-  zip -r utrains_cron_zip_$MYDATE.zip utrains_des_* utrains_arch_cron-$MYDATE 
+  zip -r utrains_cron_zip_$MYDATE.zip utrains_arch_cron-$MYDATE 
   
   rm -r utrains_arch_cron-$MYDATE
 
